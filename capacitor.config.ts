e19a5528887a6ef0779de1723eb09d81e177f288
Leonardo <leoalvesamc062gmail.com>
2025-11-18 21:12:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Formadora4',
+  webDir: 'www'
+};
+
+export default config;
